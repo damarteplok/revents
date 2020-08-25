8f6386cc87f650cbdae7e29bfe5981d4fa99115c
damarhuda <damarhuda@live.com>
2020-08-25 21:17:36 +0700

    crud

--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,20 +1,51 @@
-import React, {useState} from 'react';
-import { Grid } from 'semantic-ui-react';
-import EventList from './EventList';
-import EventForm from '../eventForm/EventForm';
-import {sampleData} from '../../../app/api/sampleData';
+import React, { useState } from "react";
+import { Grid } from "semantic-ui-react";
+import EventList from "./EventList";
+import EventForm from "../eventForm/EventForm";
+import { sampleData } from "../../../app/api/sampleData";
 
-export default function EventDashboard({formOpen, setFormOpen}) {
-    const [events, setEvents] = useState(sampleData);
-    return (
-        <Grid>
-            <Grid.Column width={10}>
-                <EventList events={events}/>
-            </Grid.Column>
-            <Grid.Column width={6}>
-                {formOpen &&
-                <EventForm setFormOpen={setFormOpen}/>}
-            </Grid.Column>
-        </Grid>
-    );
-};
+export default function EventDashboard({
+  formOpen,
+  setFormOpen,
+  selectEvent,
+  selectedEvent,
+}) {
+  const [events, setEvents] = useState(sampleData);
+
+  function handleCreateEvent(event) {
+    setEvents([...events, event]);
+  }
+
+  function handleUpdateEvent(updatedEvent) {
+    setEvents(events.map((e) => (e.id === updatedEvent.id ? updatedEvent : e)));
+    selectEvent(null);
+  }
+
+  function handleDeleteEvent(eventId) {
+    setEvents(events.filter((e) => e.id !== eventId));
+  }
+
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <EventList
+          events={events}
+          selectEvent={selectEvent}
+          deleteEvent={handleDeleteEvent}
+        />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        {formOpen && (
+          <EventForm
+            setFormOpen={setFormOpen}
+            setEvents={setEvents}
+            createEvent={handleCreateEvent}
+            selectedEvent={selectedEvent}
+            updateEvent={handleUpdateEvent}
+            key={selectedEvent ? selectedEvent.id : null}
+          />
+        )}
+      </Grid.Column>
+    </Grid>
+  );
+}
--- src/features/events/eventDashboard/EventList.jsx
+++ src/features/events/eventDashboard/EventList.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import EventListItem from "./EventListItem";
 
-export default function EventList({events}) {
+export default function EventList({ events, selectEvent, deleteEvent }) {
   return (
     <>
       {events.map((event) => {
-        return <EventListItem key={event.id} event={event} />;
+        return (
+          <EventListItem
+            key={event.id}
+            event={event}
+            selectEvent={selectEvent}
+            deleteEvent={deleteEvent}
+          />
+        );
       })}
     </>
   );
