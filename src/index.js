edf3f0b7f1a790ed96f0c2e41758b0b9ad740b7f
damarhuda <damarhuda@live.com>
2020-08-30 11:57:33 +0700

    scroll to top

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { configureStore } from "./app/store/configureStore";
+import ScrollToTop from "./app/layout/ScrollToTop";
 
 const rootEl = document.getElementById("root");
 
@@ -23,6 +24,7 @@ const store = configureStore();
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
+    <ScrollToTop />
       <App />
     </BrowserRouter>
   </Provider>,
